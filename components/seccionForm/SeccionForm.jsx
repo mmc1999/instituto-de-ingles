f271651875c57bd164e9fd34482ef35890d87a82
mmc1999 <mcentorbi161999@gmail.com>
2022-08-02 16:49:32 -0300

    cambiando las prop de mapa

--- components/seccionForm/SeccionForm.jsx
+++ components/seccionForm/SeccionForm.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Formulario from '../formulario/Formulario'
 import Ubicacion from '../ubicacion/Ubicacion';
 import FormModule from "./SeccionForm.module.css"
 
 const SeccionForm = () => {
   const [valor, setValor] = useState("")
-
-  if (typeof window !== 'undefined') {
-    setValor(window.location.pathname.slice(1))
-  }
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+        setValor(window.location.pathname.slice(1))
+    }
+  }, [valor])
+  
   return (
     <section className={FormModule.seccionPrincipal}>
         <p className={FormModule.titleForm}>Ponete en contacto con nosotros</p>
